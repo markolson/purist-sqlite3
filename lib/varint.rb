666b7185aa3872be6e513d042b486552234a773b
Mark Olson <dustgun@gmail.com>
2010-01-04 00:21:55 -0500

    duh.

--- lib/varint.rb
+++ lib/varint.rb
@@ -2,8 +2,8 @@ def varint(f)
     segments = []
     begin
       msb = f.read(1).unpack('C')[0]
-      segments << (msb & 127)
+      segments << msb
     end while (msb & 128 == 128)
     # I'm a bad, bad person.
-    result = segments.map { |x| [x].pack('C').unpack('B8')[0][1..7] }.reverse.join('').to_i(2)
+    result = segments.map { |x| [x].pack('C').unpack('B8')[0][1..7] }.join('').to_i(2)
 end
